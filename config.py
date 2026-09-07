e5248125afb288e4ed6b9cc78e3bc961d2a25daa
MarkovSergei <165809659+MarkovSergei@users.noreply.github.com>
2026-09-07 22:32:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Telegram
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+# MAX
+MAX_BOT_TOKEN = os.getenv("MAX_BOT_TOKEN", "")
+
+# Общие
 ADMIN_USER_ID = os.getenv("ADMIN_USER_ID", "")
 PORT = int(os.getenv("PORT", 8000))
 
